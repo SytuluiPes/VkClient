e346fc4a61fc67c584f3d4b95dc73aef5148cebc
artem <artemkovalev2014@gmail.com>
2024-08-24 10:36:29 +0300

    2.3 step

--- app/src/main/java/ru/artem/vkclient/MainActivity.kt
+++ app/src/main/java/ru/artem/vkclient/MainActivity.kt
@@ -3,19 +3,13 @@ package ru.artem.vkclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import ru.artem.vkclient.ui.TimesTable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Artem")
+            TimesTable(n = 7)
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
